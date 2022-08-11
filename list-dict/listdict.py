59f67bc640a7f638c58287c435428bdfe2436fcf
jacquesjg.noreply.github.com <student@bchd.6edea065-740b-46ef-a647-47269440ecab>
2022-08-11 15:12:20 +0000

    copying local files with python

---
+++ list-dict/listdict.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+foods1 = ["pizza", "pasta"]
+foods2 = ["ribs", "steak"]
+foods3 = ["wings", "burrito"]
+
+favoriteFoods = [foods1, foods2, foods3]
+
+print(favoriteFoods)
+print("My favorite food is " + favoriteFoods[1][0])
+
+dictionary = {1: "courage", 2: "valor", 3: "intelligence"}
+
+print(dictionary.keys())
+print(dictionary.values())
---
+++ scratch/osinteraction.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import shutil
+import os
+
+os.chdir("/home/student/mycode/")
+shutil.copy("asadtale.txt", "examples/copiedasadtale.txt")
+
